6f549ed3795b5e805c3f7f4fd613ef78e4a61231
Siham H <byformi@gmail.com>
2018-08-13 15:53:12 +0200

    13aug-Map

--- Unit/ViewController.swift
+++ Unit/ViewController.swift
@@ -1,23 +1,55 @@
 //
 //  ViewController.swift
-//  Nike+Research
+//  Unit
 //
-//  Created by Duc Tran on 3/19/17.
-//  Copyright © 2017 Developers Academy. All rights reserved.
+//  Created by Siham Hadi on 3/9/18.
+//  Copyright © 2018 Unit All rights reserved.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    // Map
+    @IBOutlet weak var mapView: MKMapView!
+    let manager = CLLocationManager ()
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01,0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude,
+        location.coordinate.longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        mapView.setRegion(region, animated: true)
+        
+        print(location.altitude)
+        print(location.coordinate)
+        print(location.speed)
+        
+        self.mapView.showsUserLocation = true
 
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
     }
 
 
